7f2cb14be4b8fdf2d08654c62f467a4dd3772eef
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-10-27 14:14:28 -0700

    Started Pizza Order Print, half complete

--- pizza-order-printer/pizza-order-printer.js
+++ pizza-order-printer/pizza-order-printer.js
@@ -5,3 +5,27 @@ Make a Node.js app that prints pizza orders to the console.
 
 Check the README.md file for instructions.
 ------------------------- */
+
+import fs from "fs/promises";
+
+const pizzaOrders = process.argv[2];
+console.log(pizzaOrders);
+
+// Removed async & await to get console log data first - NOTE: Must use async & await to get successful data console log to show
+
+async function getAllOrders() {
+  const data = await fs.readFile("./data.json", "utf8");
+  // console.log(data)
+  const parsedData = JSON.parse(data);
+
+  console.log(parsedData);
+}
+
+getAllOrders();
+
+async function getOneOrder() {
+  const data = await fs.readFile("./data.json", "utf8");
+  const parsedData = JSON.parse(data);
+}
+
+getOneOrder();
