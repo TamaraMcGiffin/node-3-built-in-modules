151540b8b932fda66b98f46d1ecf3738114756d9
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-10-27 10:08:36 -0700

    Created & Tested Birthstone Finder - Node Function

--- birthstone-finder/warmup-birthstone-finder.js
+++ birthstone-finder/warmup-birthstone-finder.js
@@ -21,3 +21,25 @@ const birthstones = {
   November: "Topaz or Citrine",
   December: "Tanzanite, Zircon, or Turquoise",
 };
+
+// 3rd command line in the array
+const birthMonth = process.argv[2];
+
+if (!birthMonth) {
+  console.log("Invalid month. Please enter a valid month.");
+
+  // process exit used inside of an if code block, reccomended built in function in node for handling errors and stops the node process instead of running itcontinuously 
+  process.exit(1);
+}
+
+// Birth month kept coming back invalid, adjusted code from const monthKey = birthMonth so that key months will be recognized
+const monthKey =
+  birthMonth.charAt(0).toUpperCase() + birthMonth.slice(1).toLowerCase();
+
+const birthstone = birthstones[monthKey];
+
+if (birthstone) {
+  console.log(`The birthstone for ${monthKey} is ${birthstone}.`);
+} else {
+  console.log("Invalid month. Please enter a valid month.");
+}
